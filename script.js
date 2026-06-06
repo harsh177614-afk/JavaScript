ff29ecad0b24a9cb9bb7b8c0afb9f4619e22c962
Harsh Gupta <harsh177614@gmail.com>
2026-06-06 09:46:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// variable
+var Name = "Boss";
+Name = "Himanshu";
+console.log("Himanshu")
+console.log(typeof(Name))
+
+// const 
+
+const coder = "54";
+console.log("power")
+console.log(typeof(coder))
+
+// let ->
+let Machine="laptop";
+console.log(Machine) 
+console.log(typeof(Machine))
